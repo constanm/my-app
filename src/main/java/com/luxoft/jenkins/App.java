54aeb36d551a9524df7bf708e83f4e8546f76279
ajs-ro-01@ptc.local <ajs-ro-01@ptc.local>
2019-03-01 12:34:36 +0200

    first commit of my-app

---
+++ src/main/java/com/luxoft/jenkins/App.java
@@ -0,0 +1,13 @@
+package com.luxoft.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
